d24a8169099ebb93b97c282f849da7846590d8de
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2026-08-12 20:28:19 -0300

    cors

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ import express from 'express';
 import session from 'express-session';
 import taskRoutes from './routes/taskRoutes.js'
 import authRoutes from './routes/authRoutes.js'
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "https://ericksantosl.github.io"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(session({
   secret: process.env.SESSION_SECRET,
